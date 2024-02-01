0c3a0c9245c0a61fe3ea4660246dd852543c5b02
Haitam Borqane <haytamborquane@gmail.com>
2024-02-01 19:36:02 +0100

    adding metric plotting function

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,8 @@
 import torch
 import torchinfo
 from torchinfo import summary
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def get_miscellanous_variables(train_data):
     class_names = train_data.classes
@@ -13,3 +15,9 @@ def get_device():
 
 def model_summary(model, tensor_size):
     return summary(model, input_size=tensor_size)
+
+def visualize_training_metrics(metrics, num_epochs):
+    sns.lineplot(y= metrics["train_acc"], x= [i for i in range(num_epochs)], label= "train_acc")
+    sns.lineplot(y= metrics["test_acc"], x= [i for i in range(num_epochs)], label= "test_acc")
+    plt.xlabel("epochs")
+    plt.ylabel("accuracy")
